53cf7d6b6e6c33b2576fc8495c4c11cb433c73d3
Pranit Patil <91155068+PranitPatil03@users.noreply.github.com>
2023-07-25 02:58:04 +0530

    Changes Completed

--- frontend_react/src/components/SocialMedia.js
+++ frontend_react/src/components/SocialMedia.js
@@ -1,21 +1,28 @@
-import React from 'react'
-import { BsTwitter,BsInstagram} from 'react-icons/bs'
-import { FaFacebook} from 'react-icons/fa'
+import React from "react";
+import { BsTwitter, BsInstagram } from "react-icons/bs";
+import { FaLinkedin } from "react-icons/fa";
+import { AiFillGithub } from "react-icons/ai";
 
 const SocialMedia = () => {
   return (
-    <div className='app__social'>
-        <div>
-            <BsTwitter/>
-        </div>
-        <div>
-            <BsInstagram/>
-        </div>
-        <div>
-            <FaFacebook/>
-        </div>
+    <div className="app__social">
+      <div>
+        <a href="https://twitter.com/Patilpranit3112">
+          <BsTwitter />
+        </a>
+      </div>
+      <div>
+        <a href="https://www.linkedin.com/in/patilpranit03/">
+          <FaLinkedin />
+        </a>
+      </div>
+      <div>
+        <a href="https://github.com/PranitPatil03/">
+          <AiFillGithub />
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SocialMedia
+export default SocialMedia;
